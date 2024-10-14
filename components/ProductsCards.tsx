365b40eef4a6a8e1959336c6b3bd0a8b52e9ab74
SDN33 <s.deinegri2@gmail.com>
2024-10-14 22:05:01 +0200

    meme

--- components/ProductsCards.tsx
+++ components/ProductsCards.tsx
@@ -212,7 +212,7 @@ const ProductsCards: React.FC = () => {
 
                 <p className="text-sm mb-2">{product.region}</p>
 
-                <button className="mt-auto bg-orange-600 text-white px-4 py-2 rounded hover:bg-orange-700">
+                <button className="mt-auto bg-orange-600 text-white px-4 py-2 rounded hover:bg-orange-700 max-w-fit mx-auto">
                   Ajouter au panier
                 </button>
               </div>
