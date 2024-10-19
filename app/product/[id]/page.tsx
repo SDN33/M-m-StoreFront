ac40061e50d8e98633723756907378684ab4098e
SDN33 <s.deinegri2@gmail.com>
2024-10-20 01:29:28 +0200

    mémé

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -3,7 +3,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'next/navigation';
 import Image from 'next/image';
-import { Star, Truck, Package, Video } from 'lucide-react';
+import { Star, Truck, Package } from 'lucide-react';
 import Livraison from '@/components/Livraison';
 
 interface Product {
