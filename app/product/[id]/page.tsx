c2fee87694a473ca76fa9049432a4f0bd3793895
SDN33 <s.deinegri2@gmail.com>
2024-10-19 21:53:33 +0200

    mémé

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { useRouter, useParams } from 'next/navigation';
+import { useParams } from 'next/navigation';
 import Image from 'next/image';
 import { Star, Truck, Package } from 'lucide-react';
 
