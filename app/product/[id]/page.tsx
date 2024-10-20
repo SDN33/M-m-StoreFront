c7f4e3a98e47e43fd1f442fbad475c07f429485d
SDN33 <s.deinegri2@gmail.com>
2024-10-20 18:01:29 +0200

    mémé

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -163,7 +163,7 @@ const ProductPage: React.FC = () => {
 
 
   return (
-    <div className={`mt-0 :mt-[5rem] md:px-16 sm:px-8 lg:px-16`}>
+    <div className="mt-20 px-16">
 
       <div className="relative top-0 left-0 w-full">
       </div>
