84195423496df87116c472c0c92f77a156fd1f6a
SDN33 <s.deinegri2@gmail.com>
2024-10-22 21:14:41 +0200

    mémé

--- app/vendor/[id]/page.tsx
+++ app/vendor/[id]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
-import { Wine, MapPin, Star, Award } from 'lucide-react';
+import { Wine, MapPin, Star} from 'lucide-react';
 import Image from 'next/image';
 
 // Composants de base personnalisés
