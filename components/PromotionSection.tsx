a374c5730ee4ebbc8fd8c3cde16dcbde7ed89b5e
SDN33 <s.deinegri2@gmail.com>
2024-10-24 01:54:41 +0200

    mémé

--- components/PromotionSection.tsx
+++ components/PromotionSection.tsx
@@ -26,12 +26,12 @@ const PromotionSection = () => {
     >
       <p className="font-extrabold bg-black shadow-sm text-white text-xs sm:text-xs lg:text-sm">
         <span className="sm:hidden">
-        Livraison offerte<br /> Dès 6 bouteilles achetées sur un même domaine
+        Livraison <span className='text-primary'>offerte</span><br />Dès 6 bouteilles achetées sur un même domaine
         </span>
 
         {/* Affichage sur plus grand écran sans saut de ligne */}
         <span className="hidden sm:inline text-base">
-        Livraison offerte dès 6 bouteilles achetées sur un même domaine
+        Livraison <span className='text-primary'>OFFERTE</span> dès 6 bouteilles achetées sur un même domaine
         </span>
       </p>
     </div>
