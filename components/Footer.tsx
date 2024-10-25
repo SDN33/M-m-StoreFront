b07deaf4a963dd3206c0afd9e362fee23a925182
SDN33 <s.deinegri2@gmail.com>
2024-10-25 13:05:53 +0200

    mémé

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <footer className="bg-white shadow-lg text-white py-2 text-center">
-      
+
       <div className="max-w-7xl mx-auto px-4">
         {/* Section principale */}
         <div className="mb-12 h-auto w-auto">
@@ -24,7 +24,7 @@ const Footer = () => {
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-12 max-w-4xl mx-auto text-black">
           <div>
             <h3 className=" font-semibold mb-4 text-lg text-primary">À propos de Mémé Georgette</h3>
-            <ul className="space-y-2 font-semibold">
+            <ul className="space-y-2 font-normal">
               <li><a href="https://www.memegeorgette.com/" className=" hover:text-primary transition-colors text-sm">Découvrir Mémé Georgette</a></li>
               <li><a href="/demarche" className=" hover:text-primary transition-colors text-sm">Notre Démarche</a></li>
               <li><a href="/actualites" className=" hover:text-primary transition-colors text-sm">On parle de nous !</a></li>
@@ -33,7 +33,7 @@ const Footer = () => {
 
           <div>
             <h3 className=" font-semibold mb-4 text-lg text-primary">Découvrez nos vins</h3>
-            <ul className="space-y-2 font-semibold">
+            <ul className="space-y-2 font-normal">
               <li><a href="/products/category/rouge" className="text-black hover:text-primary transition-colors text-sm">Les rouges</a></li>
               <li><a href="/products/category/blanc" className="text-black hover:text-primary transition-colors text-sm">Les blancs</a></li>
               <li><a href="/products/category/rose" className="text-black hover:text-primary transition-colors text-sm">Les rosés</a></li>
@@ -44,7 +44,7 @@ const Footer = () => {
 
           <div>
             <h3 className="font-semibold mb-4 text-lg text-primary">Besoin d&apos;aide ?</h3>
-            <ul className="space-y-2 font-semibold">
+            <ul className="space-y-2 font-normal">
               <li><a href="/contact" className="text-black hover:text-primary transition-colors text-sm">Nous contacter</a></li>
               <li><a href="/faq" className="text-black hover:text-primary transition-colors text-sm">FAQ (Foire aux questions)</a></li>
               <li><a href="/mentions-legales" className="text-black hover:text-primary transition-colors text-sm">Mentions légales</a></li>
@@ -71,11 +71,11 @@ const Footer = () => {
         </div>
         {/* Mentions légales et copyright */}
         <div className="pt-4">
-          <p className="text-xs text-black mb-4 font-semibold">
+          <p className="text-xs text-black mb-4 font-normal">
             L&apos;abus d&apos;alcool est dangereux pour la santé, sachez consommer avec modération.
             Interdiction de vente de boissons alcooliques aux mineurs de -18 ans.
           </p>
-          <p className="font-semibold text-black text-xs">
+          <p className="font-normal text-black text-xs">
             © 2024 Les Vins de Mémé Georgette, © Mémé Georgette, tous droits réservés
             <br />Création de <a className="font-bold text-primary" href="https://stillinov.com">Still-inov Agency</a>
           </p>
