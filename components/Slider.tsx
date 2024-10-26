fb0ef77570c57cab333f2b19ebd47f6fc1542999
SDN33 <s.deinegri2@gmail.com>
2024-10-26 02:08:27 +0200

    mémé

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -7,40 +7,19 @@ const Slider = () => {
   const slides = [
     {
       id: 1,
-      imageUrl: '/images/slider3.png',
-      title: 'Mémé Georgette',
-      subtitle: 'bienfait pour nous !',
-      description: '',
-      discount: '',
-      link: '/vendor/mémégeorgette'
+      imageUrl: '/images/slider1.png',
+      link: '/vendor/mémégeorgette' // Lien pour la quatrième diapositive
     },
-
     {
       id: 2,
       imageUrl: '/images/slider2.png',
-      title: 'Nos vins rouges',
-      subtitle: 'Une sélection de rouges bio et naturels',
-      description: 'Mémé connait ses vins !',
-      discount: '-10%',
-      link: '/rouge'
+      link: '/vendor/mémégeorgette' // Lien pour la troisième diapositive
     },
-    {
-      id: 3,
-      imageUrl: '/images/slider1.png',
-      title: 'Château Moulin De Peyronin',
-      subtitle: 'Vignoble Bordelais eco-responsable',
-      description: 'bienfait pour nous !',
-      discount: '-10%',
-      link: '/vendor/mémégeorgette'
-    }
-
-
   ];
 
   const nextSlide = () => {
     setCurrentSlide((prev) => (prev === slides.length - 1 ? 0 : prev + 1));
   };
-
 
   useEffect(() => {
     const timer = setInterval(nextSlide, 12000);
@@ -70,25 +49,12 @@ const Slider = () => {
               <div className="absolute inset-0 bg-gradient-to-r from-black/20 to-transparent" />
             </div>
 
-            {/* Content */}
-            <div className="relative h-full container mx-auto flex items-center px-8">
-              <div className="text-white max-w-2xl shadow-sm">
-                <h2 className=" flex text-4xl font-serif">{slide.title}</h2>
-                <h3 className="text-xl mb-2 font-serif">{slide.subtitle}</h3>
-                <p className="text-lg mb-6">{slide.description}</p>
-
-                <div className="flex items-center gap-6">
-                  <Link
-                    href={slide.link}
-                    className="bg-white text-black px-6 py-3 rounded-md text-sm font-semibold
-                           hover:text-primary hover-animate transition-colors duration-300
-                             shadow-lg hover:shadow-xl"
-                  >
-                    Voir plus
-                  </Link>
-                </div>
-              </div>
-            </div>
+            {/* Lien cliquable sur toute la diapositive */}
+            <Link
+              href={slide.link}
+              className="absolute inset-0"
+              aria-label={`Aller à ${slide.link}`}
+            />
           </div>
         ))}
       </div>
