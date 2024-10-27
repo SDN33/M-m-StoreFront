6dfd4f311a09679ac558b0491c2a6dd37293754e
SDN33 <s.deinegri2@gmail.com>
2024-10-27 05:49:42 +0100

    mémé

--- app/page.tsx
+++ app/page.tsx
@@ -41,8 +41,7 @@ export default function Home() {
 
   useEffect(() => {
     async function fetchProducts() {
-      const response = await fetch('/api/products');
-      const data = await response.json();
+      await fetch('/api/products');
     }
     fetchProducts();
   }, []);
