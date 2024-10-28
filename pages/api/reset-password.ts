8d1c0af3eac2ba3be1ddbd223d23ec6e04d3a437
SDN33 <s.deinegri2@gmail.com>
2024-10-29 00:08:03 +0100

    mémé

--- pages/api/reset-password.ts
+++ pages/api/reset-password.ts
@@ -1,5 +1,4 @@
 import axios, { AxiosError } from 'axios';
-
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
