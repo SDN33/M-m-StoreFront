bf7d499647b792492b3bd8429af0fc1d86d5e667
SDN33 <s.deinegri2@gmail.com>
2024-10-29 15:47:24 +0100

    mémé

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ShoppingCart, User, Menu as MenuIcon, X, ChevronDown, ChevronRight, ChevronLeft } from 'lucide-react';
+import { User, Menu as MenuIcon, X, ChevronDown, ChevronRight, ChevronLeft } from 'lucide-react';
 import Image from 'next/image';
 import { useState } from 'react';
 import CartPopup from './CartPopup';
