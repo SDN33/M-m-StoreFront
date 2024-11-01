51464dbf49d10f40eaff94f4aa73137689425351
SDN33 <s.deinegri2@gmail.com>
2024-11-01 19:20:24 +0100

    mémé

--- components/Header.tsx
+++ components/Header.tsx
@@ -187,11 +187,11 @@ const Header = () => {
           <div
             className="fixed z-50 inset-0 flex items-center justify-center p-4"
           >
-            <div className="relative bg-white p-6 rounded-lg shadow-xl w-96 max-h-180 overflow-y-auto">
+            <div className="relative bg-slate-200 p-6 rounded-lg shadow-xl w-96 max-h-180 overflow-y-auto">
               <button className="absolute top-2 right-2" onClick={toggleNosVinsPopup}>
                 <X className="w-6 h-6 text-gray-600" />
               </button>
-              <h2 className="text-lg font-bold mb-4 text-primary">Nos Vins</h2>
+              <h2 className="text-lg font-bold mb-4">Nos Vins</h2>
 
               {/* Vignettes des catégories de vin */}
               <div className="grid grid-cols-2 gap-4">
@@ -203,10 +203,10 @@ const Header = () => {
                       backgroundImage: `url(${subcategory.backgroundImage})`,
                       backgroundSize: 'fill',
                       backgroundPosition: 'center',
-                      height: '100px', // Hauteur des vignettes
+                      height: '150px', // Hauteur des vignettes
                     }}
                   >
-                    <a href={`/products/category/${subcategory.name.toLowerCase()}`} className="absolute inset-0 flex items-center justify-center text-white font-bold bg-black bg-opacity-30 hover:bg-opacity-40 transition duration-300">
+                    <a href={`/products/category/${subcategory.name.toLowerCase()}`} className="absolute inset-0 flex items-center justify-center text-white border-2 border-primary font-bold bg-black bg-opacity-30 hover:bg-opacity-40 transition duration-300">
                       {subcategory.name}
                     </a>
                   </div>
