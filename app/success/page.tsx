4321287be1e759248b5d72f9ad6064b6a8052895
SDN33 <s.deinegri2@gmail.com>
2024-11-10 17:22:40 +0100

    dl

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,8 +1,8 @@
-'use client'; // Assurez-vous que ce fichier est un composant client
+'use client'; // Marque ce fichier comme un composant client
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, Suspense } from 'react';
 import { CheckCircle2, Home } from 'lucide-react';
-import { useSearchParams } from 'next/navigation'; // Utiliser useSearchParams depuis next/navigation
+import { useSearchParams } from 'next/navigation'; // Utiliser useSearchParams de next/navigation ici
 
 const SuccessPage = () => {
   const [loading, setLoading] = useState(true);
