ea7bd1801c1ed8ab5e4afc4237b6d18297fdc31e
SDN33 <s.deinegri2@gmail.com>
2024-11-18 11:52:50 +0100

    dl

--- components/PromotionSection.tsx
+++ components/PromotionSection.tsx
@@ -7,12 +7,12 @@ const PromotionSection = () => {
     >
       <p className="font-extrabold bg-black shadow-sm text-white text-xs sm:text-xs lg:text-sm">
         <span className="sm:hidden">
-        Livraison <span className='text-accent'>OFFERTE</span><br /><span className='text-xs'> en point relais offerte dès 6 bouteilles <br /> d&apos;un même domaine achetés</span>
+        Livraison <span className='text-accent'>OFFERTE</span><br /><span className='text-xs'> en point relais dès 6 bouteilles <br /> d&apos;un même domaine achetés</span>
         </span>
 
         {/* Affichage sur plus grand écran sans saut de ligne */}
         <span className=" hidden sm:inline text-base fade-in-up ">
-        Livraison <span className='text-accent'>OFFERTE</span><span className='text-xs'>  en point relais offerte dès 6 bouteilles d&apos;un même domaine achetés</span>
+        Livraison <span className='text-accent'>OFFERTE</span><span className='text-xs'>  en point relais dès 6 bouteilles d&apos;un même domaine achetés</span>
         </span>
       </p>
     </div>
