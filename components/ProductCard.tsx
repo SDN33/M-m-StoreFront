face0fa4a6982cc942765fff29970e42742086a4
SDN33 <s.deinegri2@gmail.com>
2024-11-19 10:05:28 +0100

    dl

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -157,10 +157,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ product, onAddToCart }) => {
       <div className="p-3">
         <div className="flex items-start gap-1 mb-2">
           <div className="relative flex-grow">
-            <div className="absolute -top-1 left-0">
-              <Heart className="text-red-500 w-5 h-5 fill-current" />
-            </div>
-            <div className="flex items-center pl-6">
+            <div className="flex items-center">
               <h3
                 className="text-base font-bold cursor-pointer hover:underline truncate"
                 onClick={handleRedirect}
@@ -174,7 +171,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ product, onAddToCart }) => {
                 </div>
               )}
             </div>
-            <p className="text-xs text-gray-500 pl-6 truncate">
+            <p className="text-xs text-gray-500 truncate">
               {product.categories.map((category) => category.name).join(" et ")} - {product.appelation?.split(/[\s-]/).map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')} · {product.millesime} · {product.volume}
             </p>
           </div>
