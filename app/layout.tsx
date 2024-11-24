dd0a5cf062d3c6b3d5cf340c1485ce905eefe99f
SDN33 <s.deinegri2@gmail.com>
2024-11-24 12:04:39 +0100

    dl

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ type TwitterMetadata = {
 
 
 export const metadata: Metadata & { twitter: TwitterMetadata } = {
-  title: "Les vins de Mémé Georgette | 2 500 vins Bio en direct de vignerons et vigneronnes engagé(e)s | Vins Bio et Biodynamie Démeter",
+  title: "VinsMemeGeorgette.com | 2 500 vins Bio en direct de vignerons et vigneronnes engagé(e)s | Vins Bio et Biodynamie Démeter",
   description:
     "Plongez dans l’univers du vin bio, où chaque gorgée raconte une histoire de passion et de respect pour la nature. Contrairement aux idées reçues, choisir un vin bio n’est pas synonyme de prix élevé. En effet, nous vous proposons une sélection de vins d'exception à des prix abordables, tout en garantissant un impact environnemental minimal.",
   icons: {
@@ -28,7 +28,7 @@ export const metadata: Metadata & { twitter: TwitterMetadata } = {
   },
   keywords: ["Mémé Georgette, Vins Mémé Georgette, acheter vins, vinatis, les grappes, vin en ligne, acheter vins bio, acheter vins en ligne, vins bio, vins en ligne, vins naturels, vins biodynamiques, vins pas chers, vins de qualité, vins de vignerons, vins de vigneronnes, vins de France, vins du monde, vins rouges, vins blancs, vins rosés, vins pétillants, vins effervescents, vins tranquilles, vins de garde, vins de cépages, vins de terroir, vins de caractère, vins de gastronomie, vins de fête, vins de tous les jours, vins de qualité, vins de prestige, vins de collection, vins de garde, vins de Bordeaux, vins de Bourgogne, vins de Loire, vins de Rhône, vins de Provence, vins de Champagne, vins d'Alsace, vins du Languedoc, vins du Roussillon, vins du Sud-Ouest, vins du Jura, vins de Savoie, vins de Corse, vins d'Espagne, vins d'Italie, vins d'Allemagne, vins du Portugal"],
   openGraph: {
-    title: "Les vins de Mémé Georgette | 2 500 vins Bio en direct de vignerons et vigneronnes engagé(e)s | Vins Bio et Biodynamie Démeter",
+    title: "VinsMemeGeorgette.com | 2 500 vins Bio en direct de vignerons et vigneronnes engagé(e)s | Vins Bio et Biodynamie Démeter",
     description:
       "Plongez dans l’univers du vin bio, où chaque gorgée raconte une histoire de passion et de respect pour la nature. Contrairement aux idées reçues, choisir un vin bio n’est pas synonyme de prix élevé. En effet, nous vous proposons une sélection de vins d'exception à des prix abordables, tout en garantissant un impact environnemental minimal.",
     images: [
@@ -43,7 +43,7 @@ export const metadata: Metadata & { twitter: TwitterMetadata } = {
   },
   twitter: {
     card: "summary_large_image",
-    title: "Les vins de Mémé Georgette | 2 500 vins Bio en direct de vignerons et vigneronnes engagé(e)s  | Vins Bio et Biodynamie Démeter",
+    title: "VinsMemeGeorgette.com | 2 500 vins Bio en direct de vignerons et vigneronnes engagé(e)s  | Vins Bio et Biodynamie Démeter",
     description:
       "Plongez dans l’univers du vin bio, où chaque gorgée raconte une histoire de passion et de respect pour la nature. Contrairement aux idées reçues, choisir un vin bio n’est pas synonyme de prix élevé. En effet, nous vous proposons une sélection de vins d'exception à des prix abordables, tout en garantissant un impact environnemental minimal.",
     images: [
