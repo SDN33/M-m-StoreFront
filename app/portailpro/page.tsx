93d01a3ca59a41e9b26a3d0b666669a886e07894
SDN33 <s.deinegri2@gmail.com>
2024-11-26 18:37:43 +0100

    dl

--- app/portailpro/page.tsx
+++ app/portailpro/page.tsx
@@ -210,21 +210,21 @@ export default function PortailPro() {
           <p className="font-bold text-lg text-center mx-auto mt-20 mb-28  slide-in-right ">
           <span className='font-black text-3xl text-primary border-black-500'>VinsMemeGeorgette.com</span><br /><br />C&apos;est bien plus qu&apos;une marketplace ! C&apos;est une communauté de vignerons bio partageant vos valeurs et bénéficiant d&apos;une image de marque forte, reconnue par le grand public grâce à <a className='text-primary font-black'  href="https://www.memegeorgette.com/" >Mémé Georgette</a><br /><br />
           <div className="flex gap-4 justify-center items-center p-6 eshadow-lg rounded-lg">
-  <Image
-    src="https://res.cloudinary.com/daroyxenr/image/upload/ar_16:9,c_fill,e_sharpen,g_auto,q_auto:good,w_1000/v1732486602/Capture_d_%C3%A9cran_2024-11-24_231608_dp7xms.webp"
-    alt="Vins fruités"
-    width={200}
-    height={200}
-    className="flex-1 object-cover rounded-lg shadow-md transform transition-all duration-500 hover:scale-105 hover:rotate-1 hover:shadow-xl mt-4"
-  />
-  <Image
-    src="/images/meme-pas-contente.png"
-    alt="Vins fruités"
-    width={200}
-    height={200}
-    className="flex-1 object-cover rounded-lg shadow-md transform transition-all duration-500 hover:scale-105 hover:-rotate-1 hover:shadow-xl mt-4"
-  />
-</div>
+            <Image
+              src="https://res.cloudinary.com/daroyxenr/image/upload/q_auto:best/v1732486602/Capture_d_%C3%A9cran_2024-11-24_231608_dp7xms.webp"
+              alt="Vins fruités"
+              width={100}
+              height={100}
+              className="flex-1 object-cover rounded-lg shadow-md transform transition-all duration-500 hover:scale-105 hover:rotate-1 hover:shadow-xl mt-4"
+            />
+            <Image
+              src="/images/meme-pas-contente.png"
+              alt="Vins fruités"
+              width={100}
+              height={100}
+              className="flex-1 object-cover rounded-lg shadow-md transform transition-all duration-500 hover:scale-105 hover:-rotate-1 hover:shadow-xl mt-4"
+            />
+          </div>
 
         <br /><br /> Nous vous offrons un accès direct aux amateurs de vin en Europe, tout en simplifiant vos démarches avec une logistique intégrée et un accompagnement marketing personnalisé
           </p>
