399f7bfdd03d858abf218da493b0e492a186b41c
SDN33 <s.deinegri2@gmail.com>
2024-11-30 17:24:27 +0100

    dl

--- components/Slogan.tsx
+++ components/Slogan.tsx
@@ -74,7 +74,7 @@ const Slogan: React.FC = () => {
         <defs>
           <linearGradient id="grad" x1="0%" y1="0%" x2="100%" y2="0%">
             <stop offset="0%" style={{ stopColor: '#FF6B4A', stopOpacity: 1 }} />
-            <stop offset="100%" style={{ stopColor: '#0000', stopOpacity: 1 }} />
+            <stop offset="100%" style={{ stopColor: '#FF6B4A', stopOpacity: 1 }} />
           </linearGradient>
         </defs>
         <path fill="url(#grad)">
