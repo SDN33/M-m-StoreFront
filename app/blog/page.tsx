64fe495d203e49da79820fd0cad50c11d5689478
SDN33 <s.deinegri2@gmail.com>
2024-11-30 20:58:56 +0100

    dl

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -58,14 +58,8 @@ const Blog = () => {
 
   if (isLoading) {
     return (
-      <div className="flex items-center justify-center min-h-screen">
-        <div className="flex flex-col items-center">
-          <div className="loader"></div>
-          <p className="text-primary font-bold text-lg">Chargement des articles de Mémé...</p>
-        </div>
-        <br /><br />
-        <br /><br />
-        <br /><br />
+      <div className="min-h-screen bg-gray-50 flex items-center justify-center">
+        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-primary border-opacity-75"></div>
       </div>
     );
   }
