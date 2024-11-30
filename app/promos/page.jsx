7b7d20d238bf938f0125d257c6953d7673961768
SDN33 <s.deinegri2@gmail.com>
2024-11-30 03:48:36 +0100

    dl

--- app/promos/page.jsx
+++ app/promos/page.jsx
@@ -60,6 +60,8 @@ export default function ProductsPromotions() {
   return (
     <div className="flex flex-col min-h-screen">
       <main className="flex-grow container mx-auto px-4 mt-44 xl:px-40">
+        <div className='text-center text-xs mx-auto text-black mb-4'><a href="/">Accueil</a> / <strong>Promos de Mémé</strong></div>
+
         {/* Section Titre */}
         <FallingEurosPromo />
 
