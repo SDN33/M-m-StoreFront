2273bc18b624fa30f5bd6cc4162b41669c365e13
SDN33 <s.deinegri2@gmail.com>
2024-11-30 19:55:17 +0100

    dl

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -197,7 +197,7 @@ const SearchInput = () => {
   }, []); // Nettoyage du timer à la destruction du composant
 
   return (
-    <div className="relative flex-grow mx-2 max-w-xl">
+    <div className="relative flex-grow mx-3 max-w-xl">
       <div className="relative">
         <input
           ref={inputRef}
