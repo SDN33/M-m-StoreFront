7119aa834ea29162e4689e007c36857fb253c807
SDN33 <s.deinegri2@gmail.com>
2024-11-30 15:24:22 +0100

    dl

--- components/ProductsCards.tsx
+++ components/ProductsCards.tsx
@@ -211,12 +211,11 @@ const ProductsCards: React.FC<ProductsCardsProps> = ({ selectedFilters, onAddToC
         <div className="text-center p-4">Aucun produit trouvé.</div>
       ) : (
         <div className="space-y-10">
-            <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4 justify-center">
             {filteredProducts.slice(0, visibleCount).map((product) => (
               <ProductCard key={product.id} product={product} onAddToCart={async () => await onAddToCart(product.id, 1, 0)} />
             ))}
-            </div>
-
+          </div>
           {filteredProducts.length > visibleCount && (
             <div className="flex justify-center py-6 !-mt-4">
               <button
@@ -230,6 +229,7 @@ const ProductsCards: React.FC<ProductsCardsProps> = ({ selectedFilters, onAddToC
         </div>
       )}
     </div>
+    
   );
 };
 
