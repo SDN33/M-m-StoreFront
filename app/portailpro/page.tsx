ed7e5fc3b9c09eae02e6d7d98fa954aab0cd4a42
SDN33 <s.deinegri2@gmail.com>
2024-12-01 15:54:39 +0100

    dl

--- app/portailpro/page.tsx
+++ app/portailpro/page.tsx
@@ -327,8 +327,7 @@ export default function PortailPro() {
                     onClick={handleDownloadDocument}
                     className="bg-black text-white w-full py-4 rounded-xl font-medium hover:shadow-lg hover:bg-teal-950 transition-all duration-300 flex items-center justify-center space-x-2"
                     >
-                    <Download className="w-5 h-5 mr-2 group-hover:scale-110 transition-transform" />
-                    Télécharger le guide de création de produit
+                    <Download className="w-5 h-5 mr-4 group-hover:scale-110 transition-transform" />Télécharger<br />Guide de création de produit
                   </button>
                 </div>
               </div>
