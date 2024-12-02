a6d872511da4f541e9d2330c2ffb5b38ce2074f8
SDN33 <s.deinegri2@gmail.com>
2024-12-02 03:04:17 +0100

    dl

--- components/CartIcon.tsx
+++ components/CartIcon.tsx
@@ -4,36 +4,19 @@ import { useCart } from '../context/CartContext';
 
 interface CartIconProps {
   onClick?: () => void;
-  className?: string;
 }
 
-const CartIcon: React.FC<CartIconProps> = ({ onClick, className }) => {
+const CartIcon: React.FC<CartIconProps> = ({ onClick }) => {
   const { cartCount } = useCart();
+  console.log('##cartCount :>> ', cartCount);
 
   return (
-    <div className={`relative inline-block ${className}`}>
-      <div
-        onClick={onClick}
-        className="group relative cursor-pointer -mt-1"
-      >
-        <ShoppingCart
-          className="w-8 h-8 text-white group-hover:text-gray-950
-          transition-all duration-300 ease-in-out
-          transform group-hover:scale-110"
-        />
-        {cartCount > 0 && (
-          <span
-            className="absolute -top-2 -right-2
-            bg-gray-950 text-white text-xs
-            rounded-full w-5 h-5
-            flex items-center justify-center
-            animate-pulse"
-          >
-            {cartCount}
-          </span>
-        )}
-      </div>
-    </div>
+    <span className="relative pr-4">
+      <ShoppingCart onClick={onClick} className="w-7 h-7 hover:scale-105 hover:rounded-xl font-semibold cursor-pointer" />
+      <span className="absolute -top-1 right-3  bg-black text-white text-xs rounded-full w-4 h-4 flex items-center justify-center">
+        {cartCount > 0 ? cartCount : 0}
+      </span>
+    </span>
   );
 };
 
