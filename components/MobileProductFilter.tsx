24ad3623367f751fd0a95f1e51083a4b66dc5edf
SDN33 <s.deinegri2@gmail.com>
2024-12-06 21:44:46 +0100

    dl

--- components/MobileProductFilter.tsx
+++ components/MobileProductFilter.tsx
@@ -145,7 +145,6 @@ const getFilterIcon = (filterType: string) => {
     sans_sulfites_: <FlaskConicalOff className="text-teal-800 w-4 h-auto mr-2" />,
   };
 
-
   return icons[filterType] || null;
 };
 
@@ -213,10 +212,10 @@ const MobileProductFilter: React.FC<MobileProductFilterProps> = ({
 
       {/* Filter Overlay */}
       {isFilterOpen && (
-        <div className="fixed inset-0 bg-black/90 z-50 flex items-end justify-center">
+        <div className="fixed inset-0 bg-black/90 z-50 flex items-end justify-center w-64">
           <div
             ref={filterRef}
-            className="bg-white opacity-95 w-screen max-h-[90vh] rounded-t-3xl overflow-y-auto custom-scrollbar shadow-lg"
+            className="bg-white opacity-95 w-full max-w-full max-h-[70vh] rounded-t-3xl overflow-y-auto custom-scrollbar shadow-lg"
           >
             {/* Close Button */}
             <div className="flex justify-between items-center p-4 border-b border-gray-200">
@@ -235,11 +234,11 @@ const MobileProductFilter: React.FC<MobileProductFilterProps> = ({
                 onClick={isHGPrixMatch}
                 className="w-full p-3 text-center rounded-lg bg-gradient-to-r from-gray-800 via-gray-800 to-gray-950 text-white hover:opacity-90 transition-all flex items-center justify-center"
               >
-                <Gift className="w-5 h-5 mr-2" /> Vins pour Offrir
+                <Gift className="w-5 h-5 mr-2 text-xs" /> Vins pour Offrir
               </button>
               <button
                 onClick={isPetitPrixMatch}
-                className="w-full p-3 text-center rounded-lg bg-gradient-to-r from-primary via-orange-800 to-red-900 text-white hover:opacity-90 transition-all"
+                className="w-full p-3 text-center text-xs rounded-lg bg-gradient-to-r from-primary via-orange-800 to-red-900 text-white hover:opacity-90 transition-all"
               >
                 Petit Budget
               </button>
