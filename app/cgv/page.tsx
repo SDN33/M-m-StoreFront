f78761cdaa333be3e0221ff4d1059b83c90e22bd
SDN33 <s.deinegri2@gmail.com>
2024-12-14 23:14:58 +0100

    dl

--- app/cgv/page.tsx
+++ app/cgv/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const CGV: React.FC = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-orange-600 px-4 py-8">
+    <div className="min-h-screen flex items-center justify-center bg-orange-600 px-4 py-8 mt-20">
       <br /><br /><br /><br />
       <div className="max-w-2xl w-fit bg-white p-6 sm:p-8 shadow-lg rounded-lg text-center">
         <h1 className="text-2xl sm:text-3xl font-bold text-center mb-4 sm:mb-6 text-orange-600">Conditions Générales de Vente (CGV)</h1>
