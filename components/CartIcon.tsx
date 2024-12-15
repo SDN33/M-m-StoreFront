ac5c24b17a068372bd47bd02579a89f16e31b0fc
SDN33 <s.deinegri2@gmail.com>
2024-12-15 18:38:25 +0100

    dl

--- components/CartIcon.tsx
+++ components/CartIcon.tsx
@@ -12,7 +12,8 @@ const CartIcon: React.FC<CartIconProps> = ({ onClick }) => {
 
   return (
     <span className="relative pr-4">
-      <ShoppingCart onClick={onClick} className="w-7 h-7 hover:scale-105 font-semibold cursor-pointer" />
+      <ShoppingCart onClick={onClick} className="w-7 h-7 hover:scale-105
+       font-semibold cursor-pointer" />
       <span className="absolute -top-1 right-3  bg-black text-white text-xs rounded-full w-4 h-4 flex items-center justify-center">
         {cartCount > 0 ? cartCount : 0}
       </span>
