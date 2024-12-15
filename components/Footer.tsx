e9bbf8576448be78b4576a43ef76e198e4720a54
SDN33 <s.deinegri2@gmail.com>
2024-12-15 18:03:51 +0100

    dl

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,11 +3,9 @@ import React from "react";
 import { CreditCard } from "lucide-react";
 import Image from "next/image";
 
-
 const Footer = () => {
   return (
-    <footer id="footer" className="bg-gradient-to-r from-gray-950 via-gray-800 to-gray-950 shadow-lg text-white py-4 text-center overflow-y-hidden">
-
+    <footer id="footer" className="bg-gradient-to-r from-gray-950 via-gray-800 to-gray-950 shadow-lg text-white py-4 text-center overflow-hidden">
       <div className="max-w-7xl mx-auto px-4">
         {/* Logos de paiement */}
         <div className="flex justify-center items-center space-x-4 w-fit h-auto mx-auto mb-8 bg-white rounded-lg p-2">
@@ -17,7 +15,7 @@ const Footer = () => {
               alt="Visa"
               height={30}
               width={30}
-              sizes="30"
+              sizes="(max-width: 768px) 30px, 30px"
             />
           </a>
           <a href="https://www.mastercard.fr" title="mastercard" aria-label="Mastercard">
@@ -26,6 +24,7 @@ const Footer = () => {
               alt="Mastercard"
               height={30}
               width={30}
+              sizes="(max-width: 768px) 30px, 30px"
             />
           </a>
           <a href="https://stripe.com/fr" title="Stripe" aria-label="Stripe">
@@ -34,6 +33,7 @@ const Footer = () => {
               alt="Stripe"
               height={50}
               width={30}
+              sizes="(max-width: 768px) 50px, 50px"
             />
           </a>
           <div className="flex items-center text-gray-600">
@@ -42,21 +42,20 @@ const Footer = () => {
           </div>
         </div>
 
-
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-8 mb-12 mt-4 max-w-4xl mx-auto text-white">
           <div>
-            <h3 className=" font-semibold mb-4 text-lg lg:text-xl text-white">À propos de Mémé...</h3>
+            <h3 className="font-semibold mb-4 text-lg lg:text-xl text-white">À propos de Mémé...</h3>
             <ul className="space-y-2 font-normal">
-              <li><a href="https://www.memegeorgette.com/" title="Site Mémé Georgette" className=" hover:text-primary transition-colors text-sm">Découvrir Mémé Georgette</a></li>
-              <li><a href="/portailpro" title="Portail Pro Mémé Georgette" className=" hover:text-primary transition-colors text-sm">Portail Pro</a></li>
-              <li><a href="/blog" title="Blog Vins Mémé Georgette" className=" hover:text-primary transition-colors text-sm">Le Blog de Mémé</a></li>
-              <li><a href="/vignerons" title="Vignerons Mémé Georgette" className=" hover:text-primary transition-colors text-sm">Nos Vignerons</a></li>
-              <li><a href="/promos" title="Promo Vins Mémé Georgette" className=" hover:text-primary transition-colors text-sm">Promos</a></li>
+              <li><a href="https://www.memegeorgette.com/" title="Site Mémé Georgette" className="hover:text-primary transition-colors text-sm">Découvrir Mémé Georgette</a></li>
+              <li><a href="/portailpro" title="Portail Pro Mémé Georgette" className="hover:text-primary transition-colors text-sm">Portail Pro</a></li>
+              <li><a href="/blog" title="Blog Vins Mémé Georgette" className="hover:text-primary transition-colors text-sm">Le Blog de Mémé</a></li>
+              <li><a href="/vignerons" title="Vignerons Mémé Georgette" className="hover:text-primary transition-colors text-sm">Nos Vignerons</a></li>
+              <li><a href="/promos" title="Promo Vins Mémé Georgette" className="hover:text-primary transition-colors text-sm">Promos</a></li>
             </ul>
           </div>
 
           <div>
-            <h3 className=" font-semibold mb-4 text-lg lg:text-xl text-white">Découvrez nos vins</h3>
+            <h3 className="font-semibold mb-4 text-lg lg:text-xl text-white">Découvrez nos vins</h3>
             <ul className="space-y-2 font-normal">
               <li><a href="/vins/rouge" className="text-white hover:text-primary transition-colors text-sm" title="Vins Rouges">Nos Vins Rouges</a></li>
               <li><a href="/vins/blanc" className="text-white hover:text-primary transition-colors text-sm" title="Vins Blancs">Nos Vins Blancs</a></li>
@@ -78,8 +77,16 @@ const Footer = () => {
         </div>
 
         {/* Mentions légales et copyright */}
-        <div className=" -mb-2">
-          <Image src="/images/image-restriction.svg" title="Prévention Alcool" alt="Prévention Alcool" width={500} height={500} className="mb-4 mx-auto w-auto h-auto" loading="lazy" />
+        <div className="-mb-2">
+          <Image
+            src="/images/image-restriction.svg"
+            title="Prévention Alcool"
+            alt="Prévention Alcool"
+            width={500}
+            height={500}
+            className="mb-4 mx-auto w-auto h-auto"
+            loading="lazy"
+          />
           <p className="text-xs text-white mb-4 font-normal">
             L&apos;abus d&apos;alcool est dangereux pour la santé, sachez consommer avec modération.
             Interdiction de vente de boissons alcooliques aux mineurs de -18 ans.
