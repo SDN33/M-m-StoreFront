0be1d588115e6e12632df7a5b40d8dd3917c4626
SDN33 <s.deinegri2@gmail.com>
2024-12-21 12:15:42 +0100

    dl

--- components/DesktopHome.tsx
+++ components/DesktopHome.tsx
@@ -87,6 +87,7 @@ export default function Home({ className }: DesktopHomeProps) {
     sans_sulfites_: [],
     petit_prix: [],
     haut_de_gamme: [],
+    prestige: [],
   }));
 
   useEffect(() => {
